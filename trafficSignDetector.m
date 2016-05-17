3a46a0d00b6b05a028d76e7640c2686c2362ab73
Doğukan Kündüm <dogukankundum@hotmail.com>
2016-05-18 02:33:23 +0300

    Update trafficSignDetector.m

--- trafficSignDetector.m
+++ trafficSignDetector.m
@@ -1,14 +1,18 @@
-
 function [] = trafficSignDetector( input_img ) % Ana Fonksiyon
 
-%[filename, pathname] = uigetfile({'*.*'}, 'Pick an Image File');
+close all;
+clear;
+clc;
+
+ %[filename, pathname] = uigetfile({'*.*'}, 'Pick an Image File');
  %oImg=imread([pathname,filename]);
- oImg = imread('C:\Users\KUNDUM\Desktop\Resimler\5.jpg');
+ oImg = imread('C:\Users\KUNDUM\Desktop\Resimler\9.jpg');
  %clcoImg = imread(input_img); % Orjinal Resmi oku
  binImg = trafficSignToBinImg(oImg); % Orjinal resmi ikilik formata çevirip gerekli iyileştirmeleri yapan fonk.
  test_vector = displayTrafficSign(binImg, oImg); % İkilik resimdeki verilere göre trafik işaretlerini bulup gösteren fonk.
 
  uzanti = '.jpg';
+ en_kucuk = 9999;
  
     for i = 1:7
     
@@ -21,6 +25,48 @@ function [] = trafficSignDetector( input_img ) % Ana Fonksiyon
         binImg = trafficSignToBinImg(veri_resim); % Orjinal resmi ikilik formata çevirip gerekli iyileştirmeleri yapan fonk.
         data_vector = displayTrafficSign(binImg, veri_resim); % İkilik resimdeki verilere göre trafik işaretlerini bulup gösteren fonk.
         
-     end
+        sonuc = test_vector - data_vector;
+        
+        sonuc = sonuc.^2;
+        
+        sum_vector = sum(sonuc);
+        
+        degerlendirme = sqrt(sum_vector);
+   
+        if (en_kucuk >= degerlendirme)
+            en_kucuk = degerlendirme;
+            data_indeks = i;
+            resim = veri_resim;
+        end
+        
+    end
+    
+    if( data_indeks == 1 )
+        h = msgbox('Yaya Geçidi','Success');
+    end
+    
+    if( data_indeks == 2 )
+        ...
+    end
+    
+    if( data_indeks == 3 )
+        ...
+    end
+    
+    if( data_indeks == 4 )
+        ...
+    end
+    
+    if( data_indeks == 5 )
+        ...
+    end
+    
+    if( data_indeks == 6 )
+        ...
+    end
+    
+    if( data_indeks == 7 )
+        ...
+    end
+     
  end
-
